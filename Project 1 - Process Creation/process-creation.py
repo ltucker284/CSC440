836150cb18412de766768c4988b75f669027093c
Liz Tucker <ltucker284@gmail.com>
2020-02-05 11:09:39 -0500

    added in print statements and todo comments

--- Project 1 - Process Creation/process-creation.py
+++ Project 1 - Process Creation/process-creation.py
@@ -1,3 +1,13 @@
 import os
 
-print('About to create a child, my pid is ' + str(os.getpid()))
+print('I\'m about to create a child process, my pid is ' + str(os.getpid()))
+
+###TODO use os.fork to create child process here
+
+print('I am a child process, my pid is ' + str(os.getpid()) + '. My ppid is ' + str(os.getppid()))
+
+###TODO make the child process sleep for 20 seconds and then exit
+
+print('The child has terminated, my pid is ' + str(os.getpid()))
+
+###TODO terminate the program
