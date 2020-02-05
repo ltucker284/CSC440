341a583be017a3986b8069315a53b28246fab659
Concerned-HumanDev <48697078+Concerned-HumanDev@users.noreply.github.com>
2020-02-05 11:45:43 -0500

    Update process-creation.py
    Use double quotes for strings longer than one word.  Also, with double quotes, you won't need to escape the single quote character.

--- Project 1 - Process Creation/process-creation.py
+++ Project 1 - Process Creation/process-creation.py
@@ -2,17 +2,17 @@ import os
 import time
 import sys
 
-print('I\'m about to create a child process, my pid is ' + str(os.getpid()))
+print("I'm about to create a child process, my pid is " + str(os.getpid()))
 
 # use os.fork to create child process here
 pid = os.fork()
 if pid == 0:
-    print('I am a child process, my pid is ' + str(os.getpid()) + '\n. My ppid is ' + str(os.getppid()))
+    print("I am a child process, my pid is " + str(os.getpid()) + "\n. My ppid is " + str(os.getppid()))
 
 # make the child process sleep for 20 seconds and then exit
 time.sleep(20)
 os._exit
-print('The child has terminated, my pid is ' + str(os.getpid()))
+print("The child has terminated, my pid is " + str(os.getpid()))
 
 # terminate the program
 sys.exit
