2ce06e900ac513139cb725004389e8405d1a13fc
Liz Tucker <ltucker284@gmail.com>
2020-03-03 11:18:21 -0500

    updates

--- Project 2 - Scheduling/scheduling.py
+++ Project 2 - Scheduling/scheduling.py
@@ -11,38 +11,40 @@ import random
 def arrival_time(list_of_processes):
     """This function iterates through a list of processes and appends process, interarrival time, and arrival time to a new list."""
     master_list = []
-    average_interarrival_time = 0
+    interarrival_time = 0
 
     for process in list_of_processes:  # Iterates through a list of 100 processes.
-        interarrival_time = 4 + random.randint(4,8)  # Generates an integer between 4 and 8, inclusive.
+        interarrival_time = random.randint(4,8)  # Generates an integer between 4 and 8, inclusive.
         if process == list_of_processes[0]:  # First process does not get an interarrival time, thus it gets a zero for this value.
             master_list.append([process, 0])
         else:
             master_list.append([process, interarrival_time])
     
     for index in range(len(master_list)):  # Iterates through a list of indexes. 
-        average_interarrival_time += master_list[index][1]  # Adds the interarrival times of each value together.
+        interarrival_time += master_list[index][1]  # Adds the interarrival times of each value together.
         if master_list[index][0] == 0:  # The first process executes immediatly, thus its arrival time is zero.
             master_list[index].append(0)
         else:
             arrival_time = master_list[index-1][2] + master_list[index][1]  # Grabs the arrival time of the previous process and adds the interarrival time of the current process.
             master_list[index].append(arrival_time)
     
-    print(f"The Average Interarrival Time Is: {str(average_interarrival_time/len(list_of_processes))}")
+    print(f"The Average Interarrival Time Is: {str(interarrival_time/len(list_of_processes))}")
 
     return master_list
 
-def service_time(list_of_processes):
+def service_time(list_of_processes, master_list):
     """This function iteratess through a list of processes, creates a dictionary with the process id as the key, and assigns the service time as the value for each key."""
     service_time_dict = {}
     average_service_time = 0
-    for process in list_of_processes:  # Iterates through a liswt of 100 processes.
-        service_time = 2 + random.randint(2,5)  # Generates an integer between 2 and 5, inclusive.
+    for process in list_of_processes:  # Iterates through a list of 100 processes.
+        service_time = random.randint(2,5)  # Generates an integer between 2 and 5, inclusive.
         service_time_dict[process] = service_time
+        master_list[process].append(service_time)
 
     for time in service_time_dict:
         average_service_time += service_time_dict[time]
     print(f"\nThe average service time is : {str(average_service_time/len(list_of_processes))}")
+    return master_list
 
 def main():
     """This process handles the script logic"""
@@ -56,9 +58,9 @@ def main():
             user_input = input("\nPrint Next Process?: (y/n) ")
             if user_input == 'n':
                 break
-    print(master_list)
 
-    service_time(list_of_processes)
+    service_time(list_of_processes, master_list)
+    print(master_list)
     
 
 
